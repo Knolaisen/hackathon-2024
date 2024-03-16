46aba6d5f75f0a1b366bd703e1eb4a6b38a5a16a
Igor Iwanicki <igor.iwanicki@protonmail.com>
2024-03-16 12:21:58 +0100

    feat: Encode xdgboost data

--- task5/xgboost_model.py
+++ task5/xgboost_model.py
@@ -15,9 +15,25 @@ def load_data(path: str) -> tuple[pd.DataFrame, pd.DataFrame]:
     X = data.iloc[:, :-1]
     y = data.iloc[:, -1]
 
-    # Testing
-    X.drop(columns=X.columns.difference(
-        ["Amount", "Origin_Amount"]), inplace=True)
+    # Encode data
+    X = pd.get_dummies(X, columns=[
+        "Origin_Currency", "Currency", "CDB_Location_Country_x",
+        "CDB_Location_CountryCode", "Opposite_party_Country",
+        "Transaction_Type", "Transaction_Location", "Deposit_Withdrawal"
+    ])
+
+    # Drop unnecessary columns
+    X.drop([
+        "Origin_Date", "System_Date", "Completed_Date",
+        'TransactionID', 'CustomerID', 'Merchant_Code', 'Opposite_party_ID',
+        'Opposite_party_Name', 'Opposite_party_Adress', 'Opposite_party_City',
+        'Transaction_Text', 'Transaction_Description_0', 'Transaction_Description_1',
+        'Transaction_Description_2'
+    ], axis=1, inplace=True)
+
+    # print(X)
+    # print(X.dtypes)
+    # exit()
 
     return X, y
 
@@ -31,9 +47,9 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 # Hyperparameter optimization
 param_grid = {
-    "max_depth": [3, 5, 7],
-    "n_estimators": [50, 100, 150],
-    "learning_rate": [0.05, 0.1, 0.2],
+    "max_depth": [3],  # , 5, 7],
+    "n_estimators": [50],  # , 100, 150],
+    "learning_rate": [0.05],  # , 0.1, 0.2],
     "subsample": [0.5],
     "colsample_bytree": [0.5],
     "gamma": [1],
